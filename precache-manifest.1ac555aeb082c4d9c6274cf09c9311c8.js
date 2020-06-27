daadbc05856fd79123b8b8f277dc6a1df1385a96
lastochkin-hub <67521971+lastochkin-hub@users.noreply.github.com>
2020-06-28 00:09:14 +0300

    Add to GitHub beacons

---
+++ precache-manifest.1ac555aeb082c4d9c6274cf09c9311c8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457e7e4d99c1c7c16504636a27ffd418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f0dfda0efe304d68cc1",
+    "url": "/static/css/main.736186fe.chunk.css"
+  },
+  {
+    "revision": "df4499f82e4121213488",
+    "url": "/static/js/2.583a809f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.583a809f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0dfda0efe304d68cc1",
+    "url": "/static/js/main.59ef9971.chunk.js"
+  },
+  {
+    "revision": "b26a18681625c4c32ac2",
+    "url": "/static/js/runtime-main.9b0602a4.js"
+  },
+  {
+    "revision": "3a636ae360603369003fc75163fa95e7",
+    "url": "/static/media/Blurring.3a636ae3.png"
+  },
+  {
+    "revision": "b5f7f697449bb201e384433bee13034c",
+    "url": "/static/media/Footer_background.b5f7f697.png"
+  },
+  {
+    "revision": "4a0e0a80970617f4d190b1341dc48653",
+    "url": "/static/media/Header_background.4a0e0a80.png"
+  },
+  {
+    "revision": "63d97994338914cbf8d41242cedcba9e",
+    "url": "/static/media/Iphone.63d97994.png"
+  },
+  {
+    "revision": "35f8dcc2f573c561abf14257caff7992",
+    "url": "/static/media/active_green_proximity.35f8dcc2.png"
+  },
+  {
+    "revision": "ddb61747855d3c16e0acc18c35c109d8",
+    "url": "/static/media/aprilbeacon.ddb61747.png"
+  },
+  {
+    "revision": "5e0e6f0713cbd8038818bd6915146aa6",
+    "url": "/static/media/asersor.5e0e6f07.png"
+  },
+  {
+    "revision": "4aedbe108e4f4218f973d8866e197994",
+    "url": "/static/media/blue_proximity.4aedbe10.png"
+  },
+  {
+    "revision": "62fe2c40945a423b3d5121fb224ba7ab",
+    "url": "/static/media/cortex.62fe2c40.png"
+  },
+  {
+    "revision": "fb12b782f5c810b7184b96717153ffae",
+    "url": "/static/media/promo.fb12b782.png"
+  },
+  {
+    "revision": "748545c253cdf9ce65515de93bccfe02",
+    "url": "/static/media/wall.748545c2.png"
+  }
+]);
